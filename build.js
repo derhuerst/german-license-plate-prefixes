e66b025ae3069dc135c0579133a9154d2e348531
Benjamin Vetter <benjamin@futureofvoice.com>
2020-05-29 13:12:02 +0200

    use new license plate data from KBA

--- build.js
+++ build.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 'use strict'
 
-const download = require('got')
 const parser = require('csv-parser')
-const fs       = require('fs')
+const fs = require('fs')
 
-
-
-const source = 'http://berlin.de/daten/liste-der-kfz-kennzeichen/kfz-kennz-d.csv'
+const source = './KFZ250.csv'
 const data = {}
 
-download.stream(source)
-.pipe(parser())
-.on('data', (entry) => {data[entry['Kennzeichen, Juli 2012']] = entry['Stadt bzw. Landkreis']})
+fs.createReadStream(source)
+.pipe(parser({"separator": ";"}))
+.on('data', (entry) => {
+	entry.KFZ.split("  ").forEach(function(char) {
+		data[char] = entry.NAME;
+	})
+})
 .on('end', _ => {
 	let file = fs.createWriteStream('./index.json')
 	file.on('finish', _ => console.log('Done.'))
