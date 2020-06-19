409e00afb37553a29dae2d1ea26d0e9fe2fef270
Jannis R <mail@jannisr.de>
2020-06-19 12:44:10 +0200

    improve build script
    - handle stream errors properly 🐛
    - exit with 1 on errors 🐛
    - check response status code & content-type header
    - find KFZ250.csv file by file name, not by absolute path
    - don't extract files that we don't need

--- build.js
+++ build.js
@@ -1,34 +1,71 @@
 #!/usr/bin/env node
 'use strict'
 
-const parser = require('csv-parser')
-const http = require('http')
-const https = require('https')
-const fs = require('fs')
+const {basename, join: pJoin} = require('path')
+const {Transform, Writable} = require('stream')
+const pump = require('pump')
 const unzipper = require('unzipper')
+const csvParser = require('csv-parser')
+const fs = require('fs')
+const https = require('https')
 
 const source = 'https://daten.gdz.bkg.bund.de/produkte/sonstige/kfz250/aktuell/kfz250.gk3.csv.zip';
-const csvSource = './kfz250_2018-09.gk3.csv/kfz250/KFZ250.csv'
-const data = {}
+const dest = pJoin(__dirname, 'index.json')
 
-https.get(source, function(response) {
-	response.pipe(unzipper.Extract({ path: '.' }))
-	response.on('end', () => {
-		try {
-			fs.createReadStream(csvSource)
-			.pipe(parser({"separator": ";"}))
-			.on('data', (entry) => {
-				entry.KFZ.split(/\s+/).forEach(function(char) {
-					data[char] = entry.NAME
-				})
+const parseAndWriteCsv = (file, cb) => {
+	const data = {}
+	const parser = new Transform({
+		objectMode: true,
+		write: (entry, _, cb) => {
+			entry.KFZ.split(/\s+/).forEach(function(char) {
+				data[char] = entry.NAME
 			})
-			.on('end', _ => {
-				let file = fs.createWriteStream('./index.json')
-				file.on('finish', _ => console.log('Done.'))
-				file.end(JSON.stringify(data))
-			})
-		} catch (e) {
-		console.error(e.message)
-		}
-	});
-});
+			cb()
+		},
+		final: (cb) => {
+			parser.push(JSON.stringify(data) + '\n')
+			cb()
+		},
+	})
+
+	pump(
+		file,
+		csvParser({separator: ';'}),
+		parser,
+		fs.createWriteStream(dest),
+		(err) => {
+			cb(err || null)
+		},
+	)
+}
+
+const onError = (err) => {
+	if (!err) return;
+	console.error(err)
+	process.exit(1)
+}
+
+https.get(source, (res) => {
+	if (res.statusCode < 200 || res.statusCode >= 300) {
+		const err = new Error('invalid HTTP status code')
+		err.res = res
+		return onError(err)
+	}
+	if (res.headers['content-type'] !== 'application/zip') {
+		const err = new Error('invalid HTTP content-type')
+		err.res = res
+		return onError(err)
+	}
+
+	const unzip = unzipper.Parse()
+	// todo [breaking]: use async iteration
+	unzip.on('entry', (file) => {
+		if (
+			file.type === 'File' &&
+			basename(file.path) === 'KFZ250.csv'
+		) parseAndWriteCsv(file, onError)
+		else file.autodrain() // discard data
+	})
+
+	pump(res, unzip, onError)
+})
